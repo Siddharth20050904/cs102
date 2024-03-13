c6a5a50dc15432fd70aaa3db90b9fb1d78150f4a
Siddharth Suryavanshi <77707661+Siddharth20050904@users.noreply.github.com>
2024-03-13 12:39:21 +0530

    first commit

---
+++ myModules/getMonth.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+var n=0;
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const d = new Date();
+var year = d.getFullYear();
+
+function getMonth(no) {
+    return new Promise((resolve, reject) => {
+        if(no=="up"){
+            n=n-1;
+        }
+        if(no=="down"){
+            n=n+1;
+        }
+        
+        
+        if(d.getMonth()+n<0){
+            year-=1;
+            n=11-d.getMonth();
+            
+        }else if(d.getMonth()+n>11){
+            year+=1;
+            n=-d.getMonth();
+        }
+
+        let name = month[d.getMonth()+n];
+        axios.get("https://calendar-json-app.adaptable.app/month/"+name+`?year=${year}`)
+            .then((response) => {
+                resolve(response.data);
+            })
+            .catch((err) => {
+                reject(err);
+            });
+    });
+}
+
+module.exports = { getMonth };
---
+++ sever.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const month = require("./myModules/getMonth");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.set('view engine','ejs');
+
+app.use(express.static("public"));
+
+app.get('/',(req,res)=>{
+    console.log(req.body)
+    month.getMonth().then((data) => {
+        if(data){
+            res.render("home",{
+                month : Object.keys(data)[0],
+                days : data[Object.keys(data)[0]]
+            })
+        }
+    });
+})
+.post('/',(req,res)=>{
+    month.getMonth(req.body.button).then((data) => {
+        if(data){
+            res.render("home",{
+                month : Object.keys(data)[0],
+                days : data[Object.keys(data)[0]],
+                year : data["year"]
+            })
+        }
+    });
+})
+
+app.listen(3000);
