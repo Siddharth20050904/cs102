369a66a38210096afacd6add08bd26f166883b9e
Siddharth20050904 <siddharthsuryavanshi0409@gmail.com>
2024-04-06 22:25:51 +0530

    Added session and cookies

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const bodyParser = require("body-parser");
 const { LogInCollection, Event } = require("./myModules/mongod");
 const monthName = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 const path = require("path");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+var passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 const app = express();
 
@@ -15,39 +19,61 @@ app.set('view engine','ejs');
 
 app.use(express.static("public"));
 
+app.use(session({
+    secret:"NOHACKERSALLOWED",
+    resave:false,
+    saveUninitialized:true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get("/",(req,res)=>{
-    res.redirect("/login");
+    if(req.isAuthenticated()){
+        console.log(req.user)
+        res.redirect("/home");
+    }else{
+        res.redirect("/login");
+    }
 })
 
 app.get('/home',(req,res)=>{
-    month.getmonthFunc().then((data) => {
-        Event.find({month:monthName.indexOf(Object.keys(data)[0])+1}).then((result)=>{
-            if(data){
-                res.render("home",{
-                    currentMonth : Object.keys(data)[0],
-                    currentDays : data[Object.keys(data)[0]],
-                    currentYear:data["year"],
-                    events: JSON.stringify(result),
-                    monthNo:monthName.indexOf(Object.keys(data)[0])+1
-                })
-            }
-        })
-    });
+    if(req.isAuthenticated()){
+        month.getmonthFunc().then((data) => {
+            Event.find({month:monthName.indexOf(Object.keys(data)[0])+1}).then((result)=>{
+                if(data){
+                    res.render("home",{
+                        currentMonth : Object.keys(data)[0],
+                        currentDays : data[Object.keys(data)[0]],
+                        currentYear:data["year"],
+                        events: JSON.stringify(result),
+                        monthNo:monthName.indexOf(Object.keys(data)[0])+1
+                    });
+                }
+            });
+        });
+    }else{
+        res.redirect("/login");
+    } 
 })
 .post('/home',(req,res)=>{
-    month.getmonthFunc(req.body.button).then((data) => {
-        Event.find({month:monthName.indexOf(Object.keys(data)[0])+1}).then((result)=>{
-            if(data){
-                res.render("home",{
-                    currentMonth : Object.keys(data)[0],
-                    currentDays : data[Object.keys(data)[0]],
-                    currentYear:data["year"],
-                    events: JSON.stringify(result),
-                    monthNo:monthName.indexOf(Object.keys(data)[0])+1
-                })
-            }
-        })
-    });
+    if(req.isAuthenticated()){
+        month.getmonthFunc(req.body.button).then((data) => {
+            Event.find({month:monthName.indexOf(Object.keys(data)[0])+1}).then((result)=>{
+                if(data){
+                    res.render("home",{
+                        currentMonth : Object.keys(data)[0],
+                        currentDays : data[Object.keys(data)[0]],
+                        currentYear:data["year"],
+                        events: JSON.stringify(result),
+                        monthNo:monthName.indexOf(Object.keys(data)[0])+1
+                    });
+                }
+            });
+        });
+    }else{
+        res.redirect("/login");
+    }
 })
 
 
@@ -84,21 +110,30 @@ app.get('/login', (req, res) => {
 
 
 app.post('/signup', async (req, res) => {
-    const data = {
-        name: req.body.name,
-        password: req.body.password,
-        rollno: req.body.rollno,
-        email: req.body.email
-    }
-    console.log(req.body)
+
     try {
-        const checking = await LogInCollection.findOne({ email: req.body.email });
+        const checking = await LogInCollection.findOne({ email: req.body.username });
 
         if (checking) {
             return res.send("User details already exist.");
         } else {
-            await LogInCollection.create(data);
-            return res.redirect("/home");
+            bcrypt.hash(req.body.password,10,async (err,hashedPassword)=>{
+                if(err){
+                    console.log(err);
+                }else{
+                    const data = {
+                        name: req.body.name,
+                        password: hashedPassword,
+                        rollno: req.body.rollno,
+                        email: req.body.email
+                    }
+                    const result = await LogInCollection.create(data);
+                    req.login(result,(err)=>{
+                        console.log(err);
+                        res.redirect("/home");
+                    });
+                } 
+            });
         }
     } catch (error) {
         console.error("Error:", error);
@@ -107,25 +142,43 @@ app.post('/signup', async (req, res) => {
 });
 
 
-app.post('/login', async (req, res) => {
+app.post('/login', passport.authenticate("local",{
+    successRedirect:"/home",
+    failureRedirect:"/login"
+}));
+
+passport.use(new LocalStrategy(async function verify(username,password,cb){
     try {
-        const check = await LogInCollection.findOne({ email: req.body.Email });
+        const check = await LogInCollection.findOne({ email: username });
 
         if (!check) {
-            return res.redirect("/login?error=user_not_found");
+            return cb("User Not Found");
         }
 
-        if (check.password !== req.body.password) {
-            return res.redirect("/login?error=incorrect_password");
-        }
-        res.redirect("/home");
-        
+        // comparing hashed password
+
+        bcrypt.compare(password,check.password, async(err,result)=>{
+            if(err){
+                return cb(err);
+            }else{
+                if(result){
+                    return cb(null,check)
+                }else{
+                    return cb(null,false);
+                }
+            }           
+        })    
     } catch (error) {
-        console.error("Error:", error);
-        res.status(500).send("Internal Server Error");
+        return cb(error)
     }
+}));
+
+passport.serializeUser((user,cb)=>{
+    cb(null,user);
 });
 
-
+passport.deserializeUser((user,cb)=>{
+    cb(null,user);
+});
 
 app.listen(3000);
